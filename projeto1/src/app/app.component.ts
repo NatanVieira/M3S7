34f9b7eba63470db60fc87c36424047240547680
Natanael Vieira <natanaelvieira130@gmail.com>
2022-10-13 14:04:05 -0300

    M3S7 - Exercicio 03

--- projeto1/src/app/app.component.ts
+++ projeto1/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projeto1';
+  
+  controle1 : FormControl;
+  controle2: FormControl;
+  controle3: FormControl;
+  controle4 : FormControl;
+  grupo1: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){
+
+    this.controle1 = formBuilder.control({value: 'nome', disabled: true});
+    this.controle2 = formBuilder.control({value: 'sobrenome', disabled: false});
+    this.controle3 = formBuilder.control({value: 'email', disabled: true});
+    this.controle4 = formBuilder.control({value:'feedback',disabled:true});
+    this.grupo1 = formBuilder.group({
+      controller: this.controle1,
+      controller2: this.controle2,
+      controller3: this.controle3,
+      controller4: this.controle4
+    });
+  }
+
 }
