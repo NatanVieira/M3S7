d8671950756923a1ff2be38412ea4fd41bd48101
Natanael Vieira <natanaelvieira130@gmail.com>
2022-10-14 09:49:04 -0300

    M3S7 - Exercicio 06

--- projeto2/src/app/app.component.ts
+++ projeto2/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +10,19 @@ export class AppComponent {
   title = 'projeto2';
 
   cadastroMateria = new FormGroup({
-    nome: new FormControl(),
-    escola: new FormControl(),
-    cargaHoraria: new FormControl(),
+    nome: new FormControl([ Validators.minLength(3), Validators.required]),
+    escola: new FormControl([Validators.required, Validators.minLength(3)]),
+    cargaHoraria: new FormControl([Validators.required, Validators.max(100), Validators.min(30)]),
     modalidade: new FormGroup({
-      opcional: new FormControl(),
-      obrigatoria: new FormControl()
+      opcional: new FormControl(Validators.required),
+      obrigatoria: new FormControl(Validators.required)
     }),
     professor: new FormGroup({
-      professor: new FormControl()
+      professor: new FormControl(Validators.required)
     })
   });
+
+  resetaForms(){
+    this.cadastroMateria.reset();
+  }
 }
